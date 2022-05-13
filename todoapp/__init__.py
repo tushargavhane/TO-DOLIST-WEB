430243012b7a05d98a3d79a96a38e8e972a15adb
Tushar <tusharg801@gmail.com>
2022-05-13 22:25:57 +0530

    Done

---
+++ todoapp/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+
+from .main.routes import main
+
+from .extensions import mongo
+
+def create_app():
+    app=Flask(__name__)
+
+    app.config['MONGO_URI'] = 'mongodb+srv://shubham:Pass123@cluster0.fsije.mongodb.net/mydb?retryWrites=true&w=majority'
+
+    mongo.init_app(app)
+
+    app.register_blueprint(main)
+
+    return app
